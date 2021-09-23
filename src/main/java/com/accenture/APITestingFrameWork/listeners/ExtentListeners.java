8f5d36215692810b1c68b2a358e4e798c4a8df2b
SunilQaAutomation <qapractise@gmail.com>
2021-09-23 21:24:14 +0530

    java mail api is updated

--- src/main/java/com/accenture/APITestingFrameWork/listeners/ExtentListeners.java
+++ src/main/java/com/accenture/APITestingFrameWork/listeners/ExtentListeners.java
@@ -1,13 +1,21 @@
 package com.accenture.APITestingFrameWork.listeners;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
 import org.testng.ISuite;
+import org.testng.ISuiteListener;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.accenture.APITestingFrameWork.utility.MonitoringMail;
+import com.accenture.APITestingFrameWork.utility.TestConfig;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
@@ -15,8 +23,7 @@ import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.Markup;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 
-
-public class ExtentListeners implements ITestListener {
+public class ExtentListeners implements ITestListener,ISuiteListener {
 	static Date d = new Date();
 	static String fileName = "Extent_" + d.toString().replace(":", "_").replace(" ", "_") + ".html";
 
@@ -45,14 +52,14 @@ public class ExtentListeners implements ITestListener {
 
 	public void onTestFailure(ITestResult result) {
 
-		//testReport.get().fail(result.getThrowable().getMessage().toString());
+		// testReport.get().fail(result.getThrowable().getMessage().toString());
 
 		String excepionMessage = Arrays.toString(result.getThrowable().getStackTrace());
 		String errorMessage = result.getThrowable().getMessage();
-		
+
 		testReport.get()
 				.fail("<details>" + "<summary>" + "<b>" + "<font color=" + "red>" + "Exception Occured:Click to see"
-						+ "</font>" + "</b >" + "</summary>" + errorMessage.replaceAll(",", "<br>") + "\n" 
+						+ "</font>" + "</b >" + "</summary>" + errorMessage.replaceAll(",", "<br>") + "\n"
 						+ excepionMessage.replaceAll(",", "<br>") + "</details> \n");
 
 		String failureLogg = "TEST CASE FAILED";
@@ -60,7 +67,7 @@ public class ExtentListeners implements ITestListener {
 		testReport.get().log(Status.FAIL, m);
 
 	}
-	
+
 	public void onTestSkipped(ITestResult result) {
 		String methodName = result.getMethod().getMethodName();
 		String logText = "<b>" + "Test Case:- " + methodName + " Skipped" + "</b>";
@@ -93,19 +100,29 @@ public class ExtentListeners implements ITestListener {
 	}
 
 	public void onFinish(ISuite suite) {
-
-		/*
-		 * try { messageBody = "http://"+InetAddress.getLocalHost().getHostAddress()+
-		 * ":8080/job/APITestingFramework/Extent_20Reports/"+fileName; } catch
-		 * (UnknownHostException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); }
-		 * 
-		 * MonitoringMail mail = new MonitoringMail(); try {
-		 * mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to,
-		 * TestConfig.subject, messageBody); } catch (AddressException e) { // TODO
-		 * Auto-generated catch block e.printStackTrace(); } catch (MessagingException
-		 * e) { // TODO Auto-generated catch block e.printStackTrace(); }
-		 */
+		
+		    try {
+				messageBody = "http://"+InetAddress.getLocalHost().getHostAddress()+
+  ":8080/job/RestAssuredTest/ExtentReports/"+fileName;
+				
+			
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+		
+		  
+		  MonitoringMail mail = new MonitoringMail(); 
+		  try {
+			mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to,
+			  TestConfig.subject, messageBody);
+		} catch (AddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
 
 	}
 
